c8955aca5a4d0713759fdb8a6423a1d304570c9c
root <root@jeedom.rivoal.me>
2017-10-03 14:04:27 +0200

    correct plugin description

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -18,16 +18,16 @@
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
-function template_install() {
+function ze_install() {
     
 }
 
-function template_update() {
+function ze_update() {
     
 }
 
 
-function template_remove() {
+function ze_remove() {
     
 }
 
